f32645540b3589df4dd4c43eb2a0f963a5b1ec35
jebi2420 <155363852+jebi2420@users.noreply.github.com>
2024-07-27 16:57:55 +0900

    [level 1] Title: 폰켓몬, Time: 0.55 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,12 +1,10 @@
 function solution(nums) {
-    let ableNum = nums.length/2; // 가져갈 수 있는 폰켓몬 수
-    let set = new Set(nums); // 중복 제거를 위해 set 사용
-    
-    if(ableNum < set.size){
-        return ableNum;
-    }
-    return set.size;
+    const ableNum = nums.length / 2; // 가져갈 수 있는 폰켓몬 수
+    const uniqueTypes = new Set(nums); // 중복 제거를 위해 set 사용
+
+    // 최대한 다양한 종류의 폰켓몬을 선택하려면, ableNum과 uniqueTypes.size 중 작은 값을 선택
+    return Math.min(ableNum, uniqueTypes.size);
 }
 
-let nums = [3,3,3,2,2,2];
+const nums = [3, 3, 3, 2, 2, 2];
 console.log(solution(nums));
