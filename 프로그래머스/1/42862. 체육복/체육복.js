6969c1c8bd40aa886abe332170fdd151f3b13ade
jebi2420 <155363852+jebi2420@users.noreply.github.com>
2024-08-10 18:09:32 +0900

    [level 1] Title: 체육복, Time: 0.25 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,19 +1,25 @@
 function solution(n, lost, reserve) {
-  // 중복 제거
-  const newReserve = reserve.filter(res => !lost.includes(res)).sort((a, b) => a - b);
-  const newLost = lost.filter(los => !reserve.includes(los)).sort((a, b) => a - b);
-
-  newReserve.forEach(res => {
-    const front = newLost.indexOf(res - 1);
-    const back = newLost.indexOf(res + 1);
-
-    // 앞 번호 학생에게 먼저 빌려줌
-    if (front !== -1) {
-      newLost.splice(front, 1);
-    } else if (back !== -1) {
-      newLost.splice(back, 1);
+    const students = {};
+    let answer = 0;
+    for(let i = 1; i <= n; i++){
+        students[i] = 1;
     }
-  });
+    lost.forEach(number => students[number] -= 1);
+    reserve.forEach(number => students[number] += 1);
 
-  return n - newLost.length;
+    for(let i = 1; i <= n; i++){
+        if(students[i] === 2 && students[i-1] === 0){
+                students[i-1]++;
+                students[i]--;
+        } else if(students[i] === 2 && students[i+1] === 0){
+                students[i+1]++;
+                students[i]--;
+        }
+    }
+    for(let key in students){
+        if(students[key] >= 1){
+            answer++;
+        }
+    }
+    return answer;
 }
